7a1d9ba51582a62266aae186362cc600b5515b8c
user <>
2019-10-29 15:01:51 +0800

    yqi

--- power-apollo/src/main/java/com/cdsen/apollo/AppProperties.java
+++ power-apollo/src/main/java/com/cdsen/apollo/AppProperties.java
@@ -12,8 +12,12 @@ public class AppProperties {
         public static final String RABBITMQ_NAMESPACE = "rabbitmq";
 
         public static final String DIRECT_EXCHANGE_POWER = "directExchangePower";
+
         public static final String DIRECT_QUEUE_CREATE_CONSUMPTION = "directQueueCreateConsumption";
         public static final String DIRECT_KEY_CREATE_CONSUMPTION = "directKeyCreateConsumption";
+
+        public static final String DIRECT_QUEUE_CREATE_INCOME = "directQueueCreateIncome";
+        public static final String DIRECT_KEY_CREATE_INCOME = "directKeyCreateIncome";
     }
 
     @Getter
